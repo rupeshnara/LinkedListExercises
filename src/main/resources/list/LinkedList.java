8748cfccc280a4bc49381067bac19d61951e92e4
rupeshnara <rupeshnara9999@gmail.com>
2017-08-16 20:05:40 -0400

    linked list adding node

--- src/main/resources/list/LinkedList.java
+++ src/main/resources/list/LinkedList.java
@@ -1,11 +1,20 @@
 package list;
 
 public class LinkedList<T> {
-	public void add(Node<T> value) {
-		
+	private Node<T> head;
+	private Node<T> tail;
+	public void add(Node<T> nextNode) {
+		if (head != null && tail != null){
+			tail.setNextNode(nextNode);
+			tail = nextNode;
+		}else {
+			head = nextNode;
+			tail = nextNode;
+		}
 	}
 	
 	public void remove(Node<T> value) {
+		
 		
 	}
 	
